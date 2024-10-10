e0b224e7e96012560b3fd5a1c9cb886c508660d5
soeunnPark <carol0724@naver.com>
2024-10-10 20:33:58 +0900

    feat: ch09

---
+++ Ch09/src/main/java/com/org/example/abstractex/Computer.java
@@ -0,0 +1,6 @@
+package com.org.example.abstractex;
+
+public abstract class Computer {
+	public abstract void display();
+	public abstract void typing();
+}
---
+++ Ch09/src/main/java/com/org/example/abstractex/MyNoteBook.java
@@ -0,0 +1,8 @@
+package com.org.example.abstractex;
+
+public class MyNoteBook extends NoteBook{
+	@Override
+	public void typing() {
+		System.out.println("MyNoteBook typing( )");
+	}
+}
